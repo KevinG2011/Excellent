40f497732d99fbb1ecdeb3208884c9ddb385b6ed
l-lijia1 <lijia@huajiao.tv>
2017-05-09 19:44:08 +0800

    added comment

--- Excellent/Algorithm/EXSortedController.h
+++ Excellent/Algorithm/EXSortedController.h
@@ -8,7 +8,10 @@
 
 #import "EXSortedBaseController.h"
 @interface EXSortedController : EXSortedBaseController
+//选择排序
 -(void)selectionSorted;
+//插入排序
 -(void)insertionSorted;
+//希尔排序
 -(void)shellSorted;
 @end
