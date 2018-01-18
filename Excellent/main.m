b06fad0f940a96b1896cb8279d80eee8be32f519
lijia <lijia@huajiao.tv>
2018-01-18 16:26:14 +0800

    deleted BN

--- Excellent/main.m
+++ Excellent/main.m
@@ -10,6 +10,11 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+    int a[5]={1,2,3,4,5};
+    int *ptr=(int *)(&a+1);
+    
+    printf("%d,%d",*(a+1),*(ptr-1));
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
