bc77256a1c71a40e6d40ea10ab33c0cd4626388d
lijia1 <iris.lance.jia@gmail.com>
2018-01-21 23:15:16 +0800

    search number in matrix

--- Excellent/Algorithm/Search/EXSearchController.h
+++ Excellent/Algorithm/Search/EXSearchController.h
@@ -10,8 +10,24 @@
 
 @interface EXSearchController : NSObject
 @property (nonatomic, strong) NSArray<NSNumber*>* array;
+/**
+ *  暴力搜索
+ */
 - (NSInteger)bruteForceSearch:(NSNumber*)num;
+/**
+ *  二分查找法
+ */
 - (NSInteger)binarySearch:(NSNumber*)num;
+/**
+ *  OC系统二分查找法
+ */
 - (NSInteger)systemBinarySearch:(NSNumber*)num;
+/**
+ *  递归查找
+ */
 - (NSInteger)recursiveSearch:(NSNumber*)num;
+/**
+ *  二维数组查找指定数字
+ */
+bool searchNumberInOrderedMatrix(int *matrix, int rows, int columns, int number);
 @end
--- Excellent/Algorithm/Search/EXSearchController.m
+++ Excellent/Algorithm/Search/EXSearchController.m
@@ -12,6 +12,7 @@
 
 @end
 
+
 @implementation EXSearchController
 - (NSInteger)bruteForceSearch:(NSNumber*)num {
     NSInteger index = [self.array indexOfObjectPassingTest:^BOOL(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
@@ -86,4 +87,41 @@
     }];
     return [self p__recursiveSearch:num low:0 high:array.count - 1 inArray:array];
 }
+
+struct Index {
+    int row;
+    int col;
+};
+
+bool searchNumberInOrderedMatrix(int *matrix, int rows, int columns, int number) {
+    if (matrix == NULL || rows < 1 || columns < 1) {
+        return -1;
+    }
+    
+    struct Index lowIndex = {0, 0};
+    struct Index highIndex = {rows - 1, columns - 1};
+    
+    return internalSearch(matrix, &lowIndex, &highIndex, number);
+}
+
+bool internalSearch(int *matrix, struct Index *lowIdx, struct Index *highIdx, int number) {
+    if (lowIdx->row >= highIdx->row && lowIdx->col >= highIdx->col) {
+        return -1;
+    }
+    int midRow = lowIdx->row + (highIdx->row - lowIdx->row) / 2;
+    int midCol = lowIdx->col + (highIdx->col - lowIdx->col) / 2;
+    
+    if (matrix[midRow * midCol + midCol] > number) {
+        highIdx->row = midRow - 1;
+        highIdx->col = midCol - 1;
+        return internalSearch(matrix, lowIdx, highIdx, number);
+    } else if (matrix[midRow * midCol + midCol] < number) {
+        lowIdx->row = midRow + 1;
+        lowIdx->col = midCol + 1;
+        return internalSearch(matrix, lowIdx, highIdx, number);
+    } else {
+        return true;
+    }
+}
+
 @end
