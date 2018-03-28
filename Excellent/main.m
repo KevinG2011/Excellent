59dcff5227796264a1bb3b9d7e4b8aa4e4fd2dae
lijia1 <iris.lance.jia@gmail.com>
2018-03-28 23:48:00 +0800

    test atomic code.

--- Excellent/main.m
+++ Excellent/main.m
@@ -6,11 +6,30 @@
 //  Copyright © 2016年 Li Jia. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+//#import "AppDelegate.h"
+#import <Foundation/Foundation.h>
+//clang -rewrite-objc main.m
+@interface TestObject : NSObject
+@property (atomic, strong) NSString         *name;
+@end
+
+@implementation TestObject
+//@synthesize name=_name;
+//-(void)setName:(NSString *)name {
+//    @synchronized (_name) {
+//        _name = name;
+//    }
+//}
+//
+//-(NSString *)name {
+//    @synchronized (_name) {
+//        return _name;
+//    }
+//}
+@end
 
 int main(int argc, char * argv[]) {    
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
